d70d7f7adc96b9b23eb3e1a400efe4ece89d4c70
aisy <aisymuhammad@gmail.com>
2020-01-06 16:48:29 +0700

    add bottom navigation

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+
+import BottomNavigation from '../components/BottomTabNavigation';
 
 import Home from '../screens/Home'
 import Search from '../screens/Search'
@@ -8,6 +11,15 @@ import Search from '../screens/Search'
 const HomeNavigation = createStackNavigator({
   Home: Home,
   Search: Search
+}, {
+  headerMode: 'none',
 })
 
-export const AppNavigator = createAppContainer(HomeNavigation);
+const HomeBottomNavigation = createBottomTabNavigator({
+  Home: Home,
+  Search: Search,
+}, {
+  tabBarComponent: BottomNavigation
+})
+
+export const AppNavigator = createAppContainer(HomeBottomNavigation);
