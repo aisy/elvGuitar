1e973d9d09fd9f3bbaedf43d491cddea53111404
aisy <aisymuhammad@gmail.com>
2020-01-06 16:48:52 +0700

    add more menu and icon

--- src/components/BottomTabNavigation.js
+++ src/components/BottomTabNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-navigation';
 import { BottomNavigation, BottomNavigationTab, Layout, Text, Icon } from '@ui-kitten/components';
 
@@ -14,14 +15,38 @@ const BottomTabNavigation = ({ navigation }) => {
     <Icon {...style} name='home-outline' />
   );
 
+  const SearchIcon = (style) => (
+    <Icon {...style} name='search-outline' />
+  );
+
+  const UserIcon = (style) => (
+    <Icon {...style} name='person-outline' />
+  );
+
   return (
-    <SafeAreaView>
-      <BottomNavigation selectedIndex={navigation.state.index} onSelect={onSelect} appearance={"noIndicator"}>
+    <SafeAreaView style={styles.container}>
+      <BottomNavigation selectedIndex={navigation.state.index} onSelect={onSelect} appearance={"noIndicator"} >
         <BottomNavigationTab title="Home" icon={HomeIcon} />
-        <BottomNavigationTab title="Search" />
+        <BottomNavigationTab title="Search" icon={SearchIcon} />
+        <BottomNavigationTab title="Home" icon={HomeIcon} />
+        <BottomNavigationTab title="Search" icon={UserIcon} />
       </BottomNavigation>
     </SafeAreaView>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+
+    elevation: 7,
+  }
+})
 
 export default BottomTabNavigation
