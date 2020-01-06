be2ba299adfa2da6a862875338dbaba6bae7b6f1
aisy <aisymuhammad@gmail.com>
2020-01-06 10:58:04 +0700

    add component screen Search

---
+++ src/screens/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout, Text } from '@ui-kitten/components';
+
+function Search() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category='h1'>Search</Text>
+    </Layout>
+  );
+}
+
+export default Search
